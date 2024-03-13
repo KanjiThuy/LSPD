2571687ed1fd93ecb1af055f5173225b63ba1216
Kanji Thụy <158560011+KanjiThuy@users.noreply.github.com>
2024-03-13 13:53:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 
-const statusMessages = ["👑 I'm Kanji Thụy","❤️ WELCOME TO 2024","UNIFIED POLICE DEPARMENT 🏢","Đang xây dựng đồn LSPD 🚓","❤️ Love Coconut ❤️","❤️ 24012024 ❤️","Nhận setup Bot & Discord theo yêu cầu 🛠️","📞 Contact Discord: thuyalwayssmiles93"];
+const statusMessages = ["Đàng chơi City","🎉 WELCOME TO 2024 🎉","🏢 UNIFIED POLICE DEPARTMENT 🏢","⛔ LOS SANTOS POLICE DEPARTMENT ⛔","Nhận setup Bot & Discord theo yêu cầu 🛠️","📞 Contact Discord: thuyalwayssmiles93"];
 
 
 let currentIndex = 0;
